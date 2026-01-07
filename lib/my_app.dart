c82e45dc31660cb535a63d2628c0b57944a56d43
Hajat Ali <hajatbagoro@gmail.com>
2026-01-07 07:30:20 -0800

    f

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,4 @@
-import 'package:firstapp/features/authentication/screens/onboarding/onboarding.dart';
+
 import 'package:firstapp/utills/constants/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme:HAppTheme.lightTheme,
       darkTheme: HAppTheme.darkTheme,
-      home: OnboardingScreen(),
+      home: Scaffold(),
     );
   }
 }
