6176e960aa5016e0f3621c1b7c965a98a8161d6c
Hajat Ali <hajatbagoro@gmail.com>
2026-01-08 01:43:35 -0800

    work in utitities

---
+++ lib/utill/constants/colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Hcolours{
+
+Hcolours._();
+
+  static const Color primary = Color(0xff0857AD);
+
+  //text colur
+  static const Color textPrimery = Color(0xff333333);
+  static const Color textSecondary = Color(0xff6C7570);
+  static const Color textwhite = Colors.white;
+  //background colour
+
+  static const Color light = Color(0xfff6f6f6);
+  static const Color dark = Color(0xff272727);
+
+  //button colors
+  static const Color buttonPrimary = Color(0xff0857A0);
+  static const Color buttonDisabled = Color(0xffC4C4C4);
+
+  //border color
+  static const Color borderPrimary = Color(0xff090909);
+  static const Color borderSecondary = Color(0xffE6E6E6);
+
+  //error and validation color
+  static const Color error = Color(0xff032f2f);
+  static const Color success = Color(0xff388e3c);
+  static const Color warning = Color(0xfff57c00);
+  static const Color info = Color(0xff397602);
+
+  //natural shaders
+   static const Color balck = Color(0xff232323);
+   static const Color darkerGrey = Color(0xff4f4f4f);
+   static const Color darkGrey = Color(0xff939393);
+  
+}
